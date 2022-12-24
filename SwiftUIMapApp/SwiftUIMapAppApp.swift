762211f3b38d825b23459d4c22e943e4137bf64a
barrosol <lucas.barroso@impactzero.pt>
2022-12-24 00:07:51 +0000

    Initial Commit

---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by Lucas Barroso IZ on 24/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
